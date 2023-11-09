d2539531f87c3c333b1dfa42e0a60a8ef2ea3015
Rümeysa AYAZ <rumeysadurmus_@hotmail.com>
2023-11-09 16:59:13 +0300

    OOP2

---
+++ OOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rümeysa Ayaz
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Rümeysa";
+            musteri1.Soyadi = "Ayaz";
+            musteri1.TcNo = "12345678910";
+
+            //Kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+        }
+    }
+}
